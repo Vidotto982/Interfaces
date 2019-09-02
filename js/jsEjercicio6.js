7c7281e2d739279cc089e0dbacfd2f57b78f2f93
Vidotto982 <vidotto982@gmail.com>
2019-09-02 13:19:38 -0300

    Probando 6 en gh pages

--- js/jsEjercicio6.js
+++ js/jsEjercicio6.js
@@ -1,46 +1,46 @@
 let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d');      
 let imagen = new Image();
-imagen.src = "banner.jpg";
-imagen.onload = function (imagen) {
-    myDrawImageMethod(this);
-}
-function myDrawImageMethod(imagen) {
-    ctx.drawImage(imagen, 0, 0);
+imagen.crossOrigin = "anonymous";
+imagen.src ="banner.jpg"; 
+imagen.width = canvas.width;
+imagen.height= canvas.height;
+let imageData= ctx.createImageData(imagen.width,imagen.height);
+
+
+
+imagen.onload = function(){
+myDrawImageMethod(this);
 }
 
-function getRed(imageData, x, y) {
-    let index = (x + y + imageData.width) * 4;
-    return imageData.data[index + 0];
+function myDrawImageMethod(imagen){
+    ctx.drawImage(imagen,0,0);
 }
-function getGreen(imageData, x, y) {
-    let index = (x + y + imageData.width) * 4;
-    return imageData.data[index + 0];
-}
-function getBlue(imageData, x, y) {
-    let index = (x + y + imageData.width) * 4;
-    return imageData.data[index + 0];
-}
-let r = getRed;
-let g = getGreen;
-let b = getBlue;
-let a = 255;
-function filtroGris(imagen, r, g, b, a) {
-    let gris= r*0.2126+g*0.7152+b*0.0722;
-    for (let i = 0; i < canvas.height; i++){
-        for(let j= 0; j< canvas.width; j++){
-            setPixel(imagen, canvas.height, canvas.width, gris, a);
+
+function filtroGris() {
+    let r = 130;
+    let g = 130;
+    let b = 130;
+    let a = 70;
+
+    for (let x = 0; x < imagen.height*2; x++){
+        for(let y= 0; y< imagen.width*2; y++){
+
+            setPixel(imageData, x, y, r, g, b, a);
             
         }
-
+        
     }
-}
-function setPixel(imageData, x, y,gris, a) {
-    let color = (x + y * imageData.width) * 4;
-    imageData.data[color + 0] = gris;
-    imageData.data[color + 1] = gris;
-    imageData.data[color + 2] = gris;
-    imageData.data[color + 3] = a;
+    ctx.putImageData(imageData,0,0);
 
 }
+function setPixel(imageData, x, y,r,g,b, a) {
+    let index = (x + y * imageData.width) * 4;
+    imageData.data[index + 0] = r;
+    imageData.data[index + 1] = g;
+    imageData.data[index + 2] = b;
+    imageData.data[index + 3] = a;
+    
+}
+
 filtroGris();
--- jsEjercicio2.js
+++
@@ -1,4 +0,0 @@
-let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext('2d');
-    ctx.fillStyle = "red";
-    ctx.fillRect(0, 0, 75, 80);
